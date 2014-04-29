ef8ca419cbafe9686f88ca7960dcaa2d740430ea
kelly becker & Angela Yen <student@epicodus.com>
2014-04-29 16:33:21 -0700

    partial addition of rails and database - in process

---
+++ code/restaurant/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restaurant::Application.config.secret_key_base = '8963c57851ea3149057038aa4f53f277e4918cf9977f1315e07c7d1c9640a494da44aa7e3ae9f1a98a206331e05ea71789a4a67418f60a2d9edf766ac4a54137'
